6a3ae8ef5c5a30a6f541f3e2f1a633877791558c
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-06-07 23:12:53 +0530

    js Added

---
+++ JavaScript/L049 String & String Method in js.js
@@ -0,0 +1,6 @@
+var name = "shashi is my ";
+var name1 = "name";
+var string  = " string";
+var complete = `${name+name1} and i can use " this and ' `;
+var length = complete.length;
+console.log(length);
